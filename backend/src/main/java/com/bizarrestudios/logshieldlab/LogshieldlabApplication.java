b0098a93e5c53c9b9706879d0ce41c7dbfab9756
TParrish21p <tdparrish314@gmail.com>
2026-05-09 17:47:03 -0500

    Add Spring Boot backend foundation

---
+++ backend/src/main/java/com/bizarrestudios/logshieldlab/LogshieldlabApplication.java
@@ -0,0 +1,13 @@
+package com.bizarrestudios.logshieldlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogshieldlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogshieldlabApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bizarrestudios/logshieldlab/LogshieldlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizarrestudios.logshieldlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogshieldlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
